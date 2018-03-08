c655b712d74f57565ec64dfe73c95ecdfdb0484c
FrankGaoX <37091183+FrankGaoX@users.noreply.github.com>
2018-03-07 19:01:31 -0500

    Add files via upload
    Uodated

--- Production_Third_step_get_to_work/Production_Third_step_get_to_work.ino
+++ Production_Third_step_get_to_work/Production_Third_step_get_to_work.ino
@@ -1,35 +1,42 @@
 #include <Servo.h>
 Servo mysservo;  // create servo object to control a servo
 Servo myWservo;  // create servo object to control a servo
+
 //Copy Data to here
 int clock_wise_back=40;
 int clock_wise_left=56;
 int clock_wise_front=69;
 int clock_wise_right=82;
 int clock_wise_back_twin=96;
+
 int counter_clock_wise_back=90;
 int counter_clock_wise_right=77;
 int counter_clock_wise_front=62;
 int counter_clock_wise_left=48;
 int counter_clock_wise_back_twin=34;
 //
-int mysservo_maximum=170;
-int mysservo_minimum=90;
+int mysservo_maximum=126;
+int mysservo_minimum=50;
+
 //Laser
 const int ledPin = 7;  // the number of the LED pin
 //sservo
-int spos=mysservo_maximum-5;      //set the initial position to 0
+int spos=90;      //set the initial position to 90
 int scpos=mysservo_maximum-5;     // current position
 int sstep=0;     //no initial movement
-int sstepspeed=2;//sensitivity 
+int sstepspeed=1;//sensitivity 
 int sreverse=mysservo_maximum;  //reverse position of s
+int sfront=90;                  //front position of s
 int sdelay=50;
 //Wservo
 int wpos=clock_wise_front;      //set the initial position to 0
 int wcpos=60;     // current position
 int wstep=0;     //no initial movement
-int wstepspeed=2;//sensitivity 
+int wstepspeed=3;//sensitivity 
 int wdelay=50;   //movement speed conrtol, Higher mean Slower
+int overshoot_control= 0; //when releaseing the button, the wservo rotates slowlly to reach to desired position. by having the value -1 this can help to stop the wservo
+
+
 void setup() {
   mysservo.attach(8);  // attaches the TOP servo on pin 8 to the servo object
   myWservo.attach(9);  // attaches the Bottom servo on pin 9 to the servo object
@@ -44,6 +51,9 @@ void setup() {
       myWservo.write(clock_wise_front); 
       delay(1000);
 }
+
+
+
 void loop() 
 {
   char val = Serial.read();   //initial reading from serial monitor
@@ -71,8 +81,11 @@ void loop()
     else if(val=='e'){               //if val is true, then it stops.
       sstep=0;
     }
-    else if(val=='h'){              //YOU KNOW WHAT THIS IS BRO.
+    else if(val=='h'){              //Look back
       spos=sreverse;
+    }
+    else if(val=='j'){              //Look front
+     spos=sfront;
     }
    }  
   else if(scpos>mysservo_maximum or spos>mysservo_maximum){             //If position value is about to exceed the limit,bring it back to range
@@ -85,6 +98,7 @@ void loop()
   }
 //  Serial.print(scpos);
 //  Serial.println(spos);
+
  //Wservo control
   wcpos=myWservo.read();       //read the postion of servo
   myWservo.write(wpos);        //give positional command to servo motor    
@@ -99,13 +113,20 @@ void loop()
       wstep=-wstepspeed;              
     }  
     else if(val=='b'){               //if val is true, then it stops.
-      wstep=0;
+      wstep=overshoot_control;       //apply overshoot value
+      wstep=0;                       //make the change to 0
     }  
-    else if(val=='h'){              //YOU KNOW WHAT THIS IS BRO.
+    else if(val=='h'){              //Look back based on current postion clockwise ot counter clock
       if(wcpos>clock_wise_front or wcpos==clock_wise_front){
       wpos=clock_wise_back_twin;}
       else if(wcpos<clock_wise_front){
       wpos=counter_clock_wise_back_twin;}
+    }
+        else if(val=='j'){              //Look front based on current postion clockwise ot counter clock
+      if(wcpos>clock_wise_front or wcpos==clock_wise_front){
+      wpos=counter_clock_wise_front;}
+      else if(wcpos<clock_wise_front){
+      wpos=clock_wise_front;}
     }
   }
   if(wcpos>clock_wise_back_twin or wpos>clock_wise_back_twin){             //If position value is about to exceed the limit,bring it back to range
@@ -116,9 +137,10 @@ void loop()
     wpos=counter_clock_wise_back_twin;
     wcpos=counter_clock_wise_back_twin;
   }
-  Serial.print(wcpos);
-  Serial.println(wpos);
-//  
+//  Serial.print(wcpos);
+//  Serial.println(wpos);
+
 }
-//
+
+
 
